d7d557d286af14ed389b4e1c1eb4913f5b9c22a8
Masataro Asai <guicho2.71828@gmail.com>
2014-11-29 19:52:23 +0900

    quiet

--- swig/swig.sh
+++ swig/swig.sh
@@ -3,6 +3,9 @@
 locate-header (){
     echo "#include <$1>" | cpp -H -o /dev/null 2>&1 | head -n1 | cut -d ' ' -f 2
 }
+q (){
+    $@ &> /dev/null
+}
 
 name=${1%.*}
 
@@ -13,12 +16,11 @@ swig -cffi -module ${name##*/} $name.expanded 2> $name.err
 # swig -cffi -o $name.lispbody -module ${name##*/} $name.expanded 2> $name.err
 cat $name.err >&2
 
-mv $name.lisp $name.lispbody
+q mv $name.lisp $name.lispbody
 cat > $name.lisp <<EOF
 (in-package :eazy-process.swig)
-$(cat $name.lispbody)
+$(q cat $name.lispbody)
 EOF
-
 
 showlines (){
     head -n $(($2-1)) $1 | tail -n 3
@@ -27,11 +29,10 @@ showlines (){
 if [[ $(grep Error $name.err | wc -l | cut -d' ' -f 1) -gt 0 ]]
 then
     showlines $name.expanded $(grep Error $name.err | cut -d: -f2)
-    rm $name.lisp
+    q rm $name.lisp
 else
 # comment out if debug is required
-    rm $name.expanded
-    rm $name.err
+    q rm $name.err $name.expanded
 fi
-rm $name.lispbody
+q rm $name.lispbody
 
