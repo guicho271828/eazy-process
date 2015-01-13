59f88a5c7d1c650b21840cc68fbe0551c89c8037
Masataro Asai <guicho2.71828@gmail.com>
2015-01-13 20:35:45 +0900

    well, work in progress

--- src/process.lisp
+++ src/process.lisp
@@ -10,12 +10,6 @@
 ;; exec
 
 @export
-(defun kill (pid sigspec)
-  (etypecase sigspec
-    ((or symbol string) (shell-command #?"kill -s $U(sigspec) $(pid)"))
-    (fixnum  (shell-command #?"kill -n $(sigspec) $(pid)"))))
-
-@export
 (defun fd (pid num)
   "return the pathname of file descriptor in procfs. pid can be a process id or :self"
   (etypecase pid
@@ -27,34 +21,8 @@
 
 @export
 (defun pgid (&optional (pid (pid)))
-  (%getpgid pid))
+  (eazy-process.swig:getpgid pid))
 (defun setpgid (pgid &optional (pid (pid)))
-  (%setpgid pid pgid))
+  (eazy-process.swig:setpgid pid pgid))
 (defsetf pgid setpgid)
 
-;;; cgroups
-
-(defun map-line (stream)
-  (iter (for line = (read-line stream nil nil))
-        (while line)
-        (collect line)))
-
-@export
-(defun cgroup (&optional (pid (pid)))
-  (with-open-file (s #?"/proc/$(pid)/cgroup")
-    (mapcar (lambda (str)
-              (register-groups-bind
-                  (id subsystems groupname)
-                  ("([0-9]+):([^:]*):(.*)" str)
-                (list (when id (read-from-string (the string id)))
-                      (split #\, subsystems)
-                      groupname)))
-            (map-line s))))
-
-@export
-(defvar *default-cgroup*
-  (lastcar (first (cgroup))))
-
-@export
-(defun whoami ()
-  (string-trim #?"\n" (shell-command "whoami")))
