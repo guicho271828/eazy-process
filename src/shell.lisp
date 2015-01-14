f91d2fc2d662fe0947066abaddd0d5c083004299
Masataro Asai <guicho2.71828@gmail.com>
2015-01-14 16:15:59 +0900

    sh -c is part of inferior-shell

--- src/shell.lisp
+++ src/shell.lisp
@@ -2,20 +2,9 @@
 
 #|
 
-subshell implemented by fork-exec
+subshell implemented with fork-execvp
 
 |#
-
-(defparameter *interpreter* "sh -c"
-  "Command line string which is invoked in order to run the subshell. Default value is \"sh -c\".
-The value is then followed by the command specified in shell-command, e.g. ,
-when command = 'ls -la', the running command is \"sh -c 'ls -la'\".
-It provides the ability to call the interpreters like perl/AWK easily.
-
-The name/path of the interpreter must be separated by spaces from the options like '-c'.
-The process is forked, then the child process calls execvp with the name of the interpreter.
-Therefore, the actual pathname of the intepreter can be resolved using PATH environment variable.
-")
 
 (defun shell (command)
   "Asynchronously execute `command` using an interpreter, returns a process structure object.
