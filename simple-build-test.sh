2add34223e222396a63cec1141b6454d571086db
Masataro Asai <guicho2.71828@gmail.com>
2015-01-20 14:05:40 +0900

    build test

--- simple-build-test.sh
+++ simple-build-test.sh
@@ -2,6 +2,16 @@
 
 sbcl=sbcl
 ccl=ccl
+ecl=ecl
+abcl=abcl
+cmucl=cmucl
 
 $sbcl --eval "(progn (ql:quickload :eazy-process.test)(quit))"
-$ccl --eval "(progn (ql:quickload :eazy-process.test)(quit))"
+
+# all fails, sigh
+
+# $ccl --eval "(progn (ql:quickload :eazy-process.test)(quit))"
+# $ecl -eval "(progn (ql:quickload :eazy-process.test)(quit))"
+# $abcl --eval '(progn (ql:quickload :eazy-process.test)(quit))'
+# $cmucl "--eval \"(progn (ql:quickload :eazy-process.test)(quit))\""
+
