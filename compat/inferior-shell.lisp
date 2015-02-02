984d95c029c0314b1c07cf29a0ef18e3f44781ac
Masataro Asai <guicho2.71828@gmail.com>
2015-02-02 10:40:42 +0900

    compat/readme.org

--- compat/inferior-shell.lisp
+++ compat/inferior-shell.lisp
@@ -2,32 +2,6 @@
 
 #|
 
-*  inferior-shell compatibility
-
-Goal: sexp -> bash compiler in inferior-shell
-inferior-shell has the following interace
-
-run
-run/s
-run/lines
-
-there could be several possible ways to implement this feature.
-
-1. implement it with direct shell.
-
-the pros in this is that we can fuly handle the underlying processes,
-and also that the code would be fast.
-
-the cons is the rather bit of ugliness and complexity; it could not be a
-direct 1-to-1 mapping between the shell script and the s-expression.
-
-2. compile the sexp into bash script
-
-the pros in this is simplicity, and its bug-ridden aspect.
- (or at least, we can rely on everythin bash supports, including exporting,
- aliasing, etc.)
-
-the cons is that we have no access to the underlying processes.
 
 |#
 
